2112bd37f53f2862b99c5b9f09f82e17daa32d97
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-11 01:29:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
+<<<<<<< HEAD
     "Started my server using Shotgun!"
+=======
+    "Welcome to your app!!!! "
+    "The World is yours"
+>>>>>>> 587fa5b98938df3824c9244acf9006d92ca50f16
   end
 
 end
